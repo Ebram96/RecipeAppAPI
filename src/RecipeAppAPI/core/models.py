7df44ef8ec3f63b314ae3092cc05c7227d2cbd84
Ebram Shehata <ebram96@gmail.com>
2020-02-10 20:20:20 +0200

    Create UserProfile model and it's model manager.

--- src/RecipeAppAPI/core/models.py
+++ src/RecipeAppAPI/core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.model import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Model manager for UserProfile model."""
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Custom user model."""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = "email"
+
+    def __str__(self):
+        """Returns a string representation for model object."""
+        return self.email
