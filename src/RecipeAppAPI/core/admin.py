28b49e03b0931ba8f48b3da1bb1d61ba6ff36bf4
Ebram Shehata <ebram96@gmail.com>
2020-02-16 17:25:12 +0200

    Create user creation page test.

--- src/RecipeAppAPI/core/admin.py
+++ src/RecipeAppAPI/core/admin.py
@@ -18,6 +18,12 @@ class UserProfileAdmin(BaseUserAdmin):
         ),
         (_("Important dates"), {"fields": ("last_login",)})
     ]
+    add_fieldsets = [
+        (None, {
+            "classes": ("wide", ),
+            "fields": ("email", "password1", "password2")
+        })
+    ]
 
 
 admin.site.register(models.UserProfile, UserProfileAdmin)
