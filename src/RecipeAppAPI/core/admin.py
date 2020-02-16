c32021ecb1cbd75b8ca3a8b365a793cb9bcf9630
Ebram Shehata <ebram96@gmail.com>
2020-02-16 16:24:01 +0200

    Create model admin for UserProfile model.

--- src/RecipeAppAPI/core/admin.py
+++ src/RecipeAppAPI/core/admin.py
@@ -1,3 +1,13 @@
-# from django.contrib import admin
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from core import models
+
+
+class UserProfileAdmin(BaseUserAdmin):
+    """Represents the django admin customizations for our custom user model"""
+    ordering = ["id"]
+    list_display = ["email", "name"]
+
+
+admin.site.register(models.UserProfile, UserProfileAdmin)
