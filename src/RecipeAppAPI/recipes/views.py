998011b14c0879b625ef33ec7f9a0ee50a57e6d9
Ebram Shehata <ebram96@gmail.com>
2020-03-12 01:05:19 +0200

    Create a new base class for Tag and Ingredient models.

--- src/RecipeAppAPI/recipes/views.py
+++ src/RecipeAppAPI/recipes/views.py
@@ -7,37 +7,29 @@ from core.models import Tag, Ingredient
 from recipes.serializers import TagSerializer, IngredientSerializer
 
 
-class TagViewSet(viewsets.GenericViewSet,
-                 mixins.ListModelMixin,
-                 mixins.CreateModelMixin):
-    """Manage tags in database"""
+class BaseRecipeAttrViewSet(viewsets.GenericViewSet,
+                            mixins.ListModelMixin,
+                            mixins.CreateModelMixin):
+    """Base viewset for recipe models owned by a user"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
 
     def get_queryset(self):
-        """Returns only tags that the currently logged user created"""
-        return self.queryset.filter(user=self.request.user)
-
-    def perform_create(self, serializer):
-        """Set the currently logged user as creator of a tag"""
-        serializer.save(user=self.request.user)
-
-
-class IngredientViewSet(viewsets.GenericViewSet,
-                        mixins.ListModelMixin,
-                        mixins.CreateModelMixin):
-    """Manage ingredients in database"""
-    serializer_class = IngredientSerializer
-    permission_classes = (IsAuthenticated,)
-    authentication_classes = (TokenAuthentication,)
-    queryset = Ingredient.objects.all()
-
-    def get_queryset(self):
-        """Returns ingredients list of the logged in user only"""
+        """Returns a list o objects owned by current logged user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
     def perform_create(self, serializer):
-        """Set the currently logged in user as the ingredient user"""
-        return serializer.save(user=self.request.user)
+        """Sets the current logged in user as the author of the object"""
+        serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in database"""
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in database"""
+    serializer_class = IngredientSerializer
+    queryset = Ingredient.objects.all()
